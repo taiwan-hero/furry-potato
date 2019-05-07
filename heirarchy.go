d3861bdde6b3c93bdbefa44a726b6c686d1917b8
Tim Tang <tim@getlocalmeasure.com>
2019-05-08 01:33:11 +1000

    final function working

--- heirarchy.go
+++ heirarchy.go
@@ -16,14 +16,15 @@ type Heirarchy struct {
 	roles   []role
 	users   []user
 	userMap map[int64]user
-	roleMap map[int64]role
+	roleMap map[int64][]user
+	roleSub map[int64][]int64
 }
 
 func (h *Heirarchy) setRoles(r []role) {
 	h.roles = r
-	h.roleMap = make(map[int64]role)
+	h.roleSub = make(map[int64][]int64)
 	for i := 0; i < len(r); i++ {
-		h.roleMap[r[i].ID] = r[i]
+		h.roleSub[r[i].Parent] = append(h.roleSub[r[i].Parent], r[i].ID)
 	}
 }
 
@@ -33,6 +34,10 @@ func (h *Heirarchy) setUsers(u []user) {
 	for i := 0; i < len(u); i++ {
 		h.userMap[u[i].ID] = u[i]
 	}
+	h.roleMap = make(map[int64][]user)
+	for i := 0; i < len(u); i++ {
+		h.roleMap[u[i].Role] = append(h.roleMap[u[i].Role], u[i])
+	}
 }
 
 func (h *Heirarchy) getUserByID(userID int64) (user, bool) {
@@ -40,6 +45,38 @@ func (h *Heirarchy) getUserByID(userID int64) (user, bool) {
 	return u, ok
 }
 
-func (h *Heirarchy) getSubordinates(u int64) []user {
-	return h.users
+func (h *Heirarchy) getUsersByRole(roleID int64) []user {
+	out := h.roleMap[roleID]
+	return out
+}
+
+func (h *Heirarchy) getSubordinateRoles(roleID int64) []int64 {
+	var out, todo []int64
+	r, _ := h.roleSub[roleID]
+	out = r
+	todo = r
+	for len(todo) > 0 {
+		r0 := todo[0]
+		todo = todo[1:]
+		r1, ok := h.roleSub[r0]
+		if ok {
+			out = append(out, r1...)
+			todo = append(todo, r1...)
+		}
+	}
+	return out
+}
+
+func (h *Heirarchy) getSubordinates(userID int64) []user {
+	var out []user
+
+	u, ok := h.getUserByID(userID)
+	if ok {
+		subRoles := h.getSubordinateRoles(u.Role)
+		for i := 0; i < len(subRoles); i++ {
+			u := h.getUsersByRole(subRoles[i])
+			out = append(out, u...)
+		}
+	}
+	return out
 }
--- heirarchy_test.go
+++ heirarchy_test.go
@@ -17,25 +17,19 @@ type users struct {
 
 func parseRoles(fileName string) []role {
 	data, _ := ioutil.ReadFile(fileName)
-
 	var r roles
-
 	json.Unmarshal(data, &r)
-
 	return r.Roles
 }
 
 func parseUsers(fileName string) []user {
 	data, _ := ioutil.ReadFile(fileName)
-
 	var u users
-
 	json.Unmarshal(data, &u)
-
 	return u.Users
 }
 
-func TestGetUserExists(t *testing.T) {
+func TestGetUserByID(t *testing.T) {
 	h := Heirarchy{}
 	u := parseUsers("./users1.json")
 	h.setUsers(u)
@@ -48,6 +42,34 @@ func TestGetUserExists(t *testing.T) {
 		t.Errorf("got %v ; expected %v", u1, expected)
 	}
 }
+
+func TestGetSubordinateRoles(t *testing.T) {
+	h := Heirarchy{}
+	r := parseRoles("./roles1.json")
+	h.setRoles(r)
+
+	r1 := h.getSubordinateRoles(2)
+
+	expected := []int64{3, 4, 5}
+	if !reflect.DeepEqual(r1, expected) {
+		t.Errorf("got %v ; expected %v", r1, expected)
+	}
+}
+
+func TestGetUsersByRole(t *testing.T) {
+	h := Heirarchy{}
+	r := parseRoles("./roles1.json")
+	h.setRoles(r)
+	u := parseUsers("./users1.json")
+	h.setUsers(u)
+
+	actual := h.getUsersByRole(5)
+	expected := []user{{5, "Steve Trainer", 5}}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v ; expected %v", actual, expected)
+	}
+}
 func TestGetSubordinates(t *testing.T) {
 	h := Heirarchy{}
 	r := parseRoles("./roles1.json")
@@ -55,8 +77,8 @@ func TestGetSubordinates(t *testing.T) {
 	u := parseUsers("./users1.json")
 	h.setUsers(u)
 
-	actual := h.getSubordinates(0)
-	expected := u
+	actual := h.getSubordinates(3)
+	expected := []user{{2, "Emily Employee", 4}, {5, "Steve Trainer", 5}}
 
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("got %v ; expected %v", actual, expected)
