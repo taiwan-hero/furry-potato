5c9e3020e981d87d65870d216593c8afaa3954e0
Tim Tang <tim@getlocalmeasure.com>
2019-05-08 01:37:27 +1000

    add comments and remove unused

--- heirarchy.go
+++ heirarchy.go
@@ -13,23 +13,21 @@ type user struct {
 }
 
 type Heirarchy struct {
-	roles   []role
-	users   []user
 	userMap map[int64]user
 	roleMap map[int64][]user
 	roleSub map[int64][]int64
 }
 
+//populates the heirarchy context with a slice of roles
 func (h *Heirarchy) setRoles(r []role) {
-	h.roles = r
 	h.roleSub = make(map[int64][]int64)
 	for i := 0; i < len(r); i++ {
 		h.roleSub[r[i].Parent] = append(h.roleSub[r[i].Parent], r[i].ID)
 	}
 }
 
+//populates the heirarchy context with a slice of users
 func (h *Heirarchy) setUsers(u []user) {
-	h.users = u
 	h.userMap = make(map[int64]user)
 	for i := 0; i < len(u); i++ {
 		h.userMap[u[i].ID] = u[i]
@@ -40,21 +38,25 @@ func (h *Heirarchy) setUsers(u []user) {
 	}
 }
 
+//given a userID, returns the user document
 func (h *Heirarchy) getUserByID(userID int64) (user, bool) {
 	u, ok := h.userMap[userID]
 	return u, ok
 }
 
+//given a role ID, returns a slice of users with that role
 func (h *Heirarchy) getUsersByRole(roleID int64) []user {
 	out := h.roleMap[roleID]
 	return out
 }
 
+//given a role ID, returns all role ID's subordinate to it (and its subordinates)
 func (h *Heirarchy) getSubordinateRoles(roleID int64) []int64 {
 	var out, todo []int64
 	r, _ := h.roleSub[roleID]
 	out = r
 	todo = r
+	//iterate through the todo queue for more subordinate roles
 	for len(todo) > 0 {
 		r0 := todo[0]
 		todo = todo[1:]
@@ -67,6 +69,7 @@ func (h *Heirarchy) getSubordinateRoles(roleID int64) []int64 {
 	return out
 }
 
+//given a user ID, returns all users that have a subordinate role (and its subordinates)
 func (h *Heirarchy) getSubordinates(userID int64) []user {
 	var out []user
 
