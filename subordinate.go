ebbdba870b7724cec9fdfc1abff06cd35620ce08
Tim Tang <tim@getlocalmeasure.com>
2019-05-07 22:49:11 +1000

    initial commit

--- subordinate.go
+++ subordinate.go
@@ -1,1 +1,34 @@
+package heirarchy
 
+import "fmt"
+
+type role struct {
+	ID     int64  `json:"Id"`
+	Name   string `json:"Name"`
+	Parent int64  `json:"Parent"`
+}
+
+type user struct {
+	ID   int64  `json:"Id"`
+	Name string `json:"Name"`
+	Role int64  `json:"Role"`
+}
+
+type Heirarchy struct {
+}
+
+func setRoles(r []role) {
+	for i := 0; i < len(r); i++ {
+		fmt.Printf("%d: %s %d\n", r[i].ID, r[i].Name, r[i].Parent)
+	}
+}
+
+func setUsers(u []user) {
+	for i := 0; i < len(u); i++ {
+		fmt.Printf("%d: %s %d\n", u[i].ID, u[i].Name, u[i].Role)
+	}
+}
+
+func getSubordinates(u int64) []user {
+
+}
--- subordinate_test.go
+++ subordinate_test.go
@@ -1,1 +1,45 @@
+package heirarchy
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+)
+
+type roles struct {
+	Roles []role `json:"Roles"`
+}
+
+type users struct {
+	Users []user `json:"Users"`
+}
+
+func parseRoles(fileName string) []role {
+	data, _ := ioutil.ReadFile(fileName)
+
+	var r roles
+
+	json.Unmarshal(data, &r)
+
+	return r.Roles
+}
+
+func parseUsers(fileName string) []user {
+	data, _ := ioutil.ReadFile(fileName)
+
+	var u users
+
+	json.Unmarshal(data, &u)
+
+	return u.Users
+}
+
+func TestGetSubordinates(t *testing.T) {
+	r := parseRoles("./roles1.json")
+
+	setRoles(r)
+
+	u := parseUsers("./users1.json")
+
+	setUsers(u)
+}
